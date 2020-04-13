0daf3d97f81185fc9d9daa38ed977815c17b73af
Juan Kevin Trujillo <me@juankevintrujillo.com>
2020-04-13 22:38:11 +0100

    Code refactoring in Utils class

--- src/main/java/io/finbook/util/Utils.java
+++ src/main/java/io/finbook/util/Utils.java
@@ -1,23 +1,47 @@
 package io.finbook.util;
 
+import io.finbook.model.Invoice;
+
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
+import java.util.ListIterator;
 
 public class Utils {
 
-    private static String currentUser = "11111111H";
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-    public static String getCurrentUser() {
-        return currentUser;
+    public static LocalDateTime getCurrentDate(){
+        return LocalDateTime.now();
     }
 
-    public static Date getCurrentDate(){
-        LocalDateTime localDateTime = LocalDateTime.now();
-        return Date.from( localDateTime.atZone( ZoneId.systemDefault()).toInstant());
+    public static LocalDateTime getFirstDayCurrentMonth(){
+        return getCurrentDate().with(LocalTime.MIDNIGHT).withDayOfMonth(1);
+    }
+
+    public static Integer getCurrentMonth(){
+        return getCurrentDate().getDayOfMonth();
+    }
+
+    public static Integer getCurrentYear(){
+        return getCurrentDate().getDayOfYear();
+    }
+
+    public static LocalDateTime getDateOfSpecificMonth(Integer month){
+        return LocalDateTime.of(getCurrentYear(), month, 1, 0, 0);
+    }
+
+    public static LocalDate parseStringToLocalDateTime(String strToLocalDateTime){
+        // using a custom pattern (01/04/2014)
+        // String asCustomPattern = dateTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        return LocalDate.parse(strToLocalDateTime, formatter);
     }
 
     public static Date parseStringToDate(String strToDate) throws ParseException {
