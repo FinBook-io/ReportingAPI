d71466a2642d672c906168c9c75e48e06ef51ab3
Juan Kevin Trujillo <ktrujillo@inerza.com>
2020-04-28 00:13:02 +0100

    Trying ChartJS and fixed minor styles for mobile version

--- src/main/resources/public/finbook/js/dashboard_scripts.js
+++ src/main/resources/public/finbook/js/dashboard_scripts.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(function() {
     $('#datatables_list_with_agination').DataTable({
         "paging": true,
         "lengthChange": false,
@@ -8,4 +8,57 @@ $( document ).ready(function() {
         "autoWidth": false,
         "responsive": true,
     });
+
+    let pageBarChart = $('#barChart');
+    if (pageBarChart.length){
+        let areaChartData = {
+            labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label               : 'Digital Goods',
+                    backgroundColor     : 'rgba(60,141,188,0.9)',
+                    borderColor         : 'rgba(60,141,188,0.8)',
+                    pointRadius          : false,
+                    pointColor          : '#3b8bba',
+                    pointStrokeColor    : 'rgba(60,141,188,1)',
+                    pointHighlightFill  : '#fff',
+                    pointHighlightStroke: 'rgba(60,141,188,1)',
+                    data                : [28, 48, 40, 19, 86, 27, 90]
+                },
+                {
+                    label               : 'Electronics',
+                    backgroundColor     : 'rgba(210, 214, 222, 1)',
+                    borderColor         : 'rgba(210, 214, 222, 1)',
+                    pointRadius         : false,
+                    pointColor          : 'rgba(210, 214, 222, 1)',
+                    pointStrokeColor    : '#c1c7d1',
+                    pointHighlightFill  : '#fff',
+                    pointHighlightStroke: 'rgba(220,220,220,1)',
+                    data                : [65, 59, 80, 81, 56, 55, 40]
+                },
+            ]
+        };
+
+        //-------------
+        //- BAR CHART -
+        //-------------
+        let barChartCanvas = pageBarChart.get(0).getContext('2d');
+        let barChartData = jQuery.extend(true, {}, areaChartData);
+        let temp0 = areaChartData.datasets[0];
+        barChartData.datasets[0] = areaChartData.datasets[1];
+        barChartData.datasets[1] = temp0;
+
+        let barChartOptions = {
+            responsive              : true,
+            maintainAspectRatio     : false,
+            datasetFill             : false
+        };
+
+        new Chart(barChartCanvas, {
+            type: 'bar',
+            data: barChartData,
+            options: barChartOptions
+        })
+    }
+
 });
