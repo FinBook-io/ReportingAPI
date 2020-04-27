2f343037ce4d8d0df6df41cabd6903b79a342f77
Juan Kevin Trujillo <ktrujillo@inerza.com>
2020-04-27 21:11:54 +0100

    Changed styles and structure of private pages, and also added DataTables and ChartJS libraries

---
+++ src/main/resources/public/finbook/js/dashboard_scripts.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+    $('#datatables_list_with_agination').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+    });
+});
